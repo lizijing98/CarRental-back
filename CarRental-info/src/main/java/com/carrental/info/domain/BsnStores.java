205d72161685457a42a62a42c2850e7f953fcc50
lizijing <lizijing@citiccfc.ad>
2022-10-26 14:01:32 +0800

    修改日志输出和门店页搜索项

--- CarRental-info/src/main/java/com/carrental/info/domain/BsnStores.java
+++ CarRental-info/src/main/java/com/carrental/info/domain/BsnStores.java
@@ -7,120 +7,140 @@ import com.carrental.common.core.domain.BaseEntity;
 
 /**
  * 门店信息对象 bsn_stores
- * 
+ *
  * @author Li Zijing
  * @date 2022-10-26
  */
-public class BsnStores extends BaseEntity
-{
+public class BsnStores extends BaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** 门店ID */
+    /**
+     * 门店ID
+     */
     private Long storeId;
 
-    /** 门店名称 */
+    /**
+     * 门店名称
+     */
     @Excel(name = "门店名称")
     private String storeName;
 
-    /** 门店地址 */
+    /**
+     * 门店地址
+     */
     @Excel(name = "门店地址")
     private String storeAddress;
 
-    /** 店长ID */
+    /**
+     * 店长ID
+     */
     @Excel(name = "店长ID")
     private Long storeManager;
 
-    /** 所属车辆数 */
+    /**
+     * 店长名称
+     */
+    @Excel(name = "店长名称")
+    private String storeManagerName;
+
+    /**
+     * 所属车辆数
+     */
     @Excel(name = "所属车辆数")
     private Integer storeOwn;
 
-    /** 门店类型 */
+    /**
+     * 门店类型
+     */
     @Excel(name = "门店类型")
     private String storeType;
 
-    /** 门店状态 */
+    /**
+     * 门店状态
+     */
     @Excel(name = "门店状态")
     private String status;
 
-    public void setStoreId(Long storeId) 
-    {
+    public void setStoreId(Long storeId) {
         this.storeId = storeId;
     }
 
-    public Long getStoreId() 
-    {
+    public Long getStoreId() {
         return storeId;
     }
-    public void setStoreName(String storeName) 
-    {
+
+    public void setStoreName(String storeName) {
         this.storeName = storeName;
     }
 
-    public String getStoreName() 
-    {
+    public String getStoreName() {
         return storeName;
     }
-    public void setStoreAddress(String storeAddress) 
-    {
+
+    public void setStoreAddress(String storeAddress) {
         this.storeAddress = storeAddress;
     }
 
-    public String getStoreAddress() 
-    {
+    public String getStoreAddress() {
         return storeAddress;
     }
-    public void setStoreManager(Long storeManager) 
-    {
+
+    public void setStoreManager(Long storeManager) {
         this.storeManager = storeManager;
     }
 
-    public Long getStoreManager() 
-    {
+    public Long getStoreManager() {
         return storeManager;
     }
-    public void setStoreOwn(Integer storeOwn) 
-    {
+
+    public String getStoreManagerName() {
+        return storeManagerName;
+    }
+
+    public void setStoreManagerName(String storeManagerName) {
+        this.storeManagerName = storeManagerName;
+    }
+
+    public void setStoreOwn(Integer storeOwn) {
         this.storeOwn = storeOwn;
     }
 
-    public Integer getStoreOwn() 
-    {
+    public Integer getStoreOwn() {
         return storeOwn;
     }
-    public void setStoreType(String storeType) 
-    {
+
+    public void setStoreType(String storeType) {
         this.storeType = storeType;
     }
 
-    public String getStoreType() 
-    {
+    public String getStoreType() {
         return storeType;
     }
-    public void setStatus(String status) 
-    {
+
+    public void setStatus(String status) {
         this.status = status;
     }
 
-    public String getStatus() 
-    {
+    public String getStatus() {
         return status;
     }
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("storeId", getStoreId())
-            .append("storeName", getStoreName())
-            .append("storeAddress", getStoreAddress())
-            .append("storeManager", getStoreManager())
-            .append("storeOwn", getStoreOwn())
-            .append("storeType", getStoreType())
-            .append("status", getStatus())
-            .append("createBy", getCreateBy())
-            .append("createTime", getCreateTime())
-            .append("updateBy", getUpdateBy())
-            .append("updateTime", getUpdateTime())
-            .append("remark", getRemark())
-            .toString();
+        return new ToStringBuilder(this, ToStringStyle.MULTI_LINE_STYLE)
+                .append("storeId", getStoreId())
+                .append("storeName", getStoreName())
+                .append("storeAddress", getStoreAddress())
+                .append("storeManager", getStoreManager())
+                .append("storeManagerName", getStoreManagerName())
+                .append("storeOwn", getStoreOwn())
+                .append("storeType", getStoreType())
+                .append("status", getStatus())
+                .append("createBy", getCreateBy())
+                .append("createTime", getCreateTime())
+                .append("updateBy", getUpdateBy())
+                .append("updateTime", getUpdateTime())
+                .append("remark", getRemark())
+                .toString();
     }
 }
